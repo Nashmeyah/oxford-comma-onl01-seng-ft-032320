ee00fa9d52d3566c097c19cc349b342b61db953f
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-03-25 18:57:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
-  if array.length ==1
+  if array.length == 1
     return "#{array[0]}"
-  elsif array.length ==2
+  elsif array.length == 2
     return array.join(" and ")
-  elsif array >= 3
+  elsif array.length >= 3
     array[-1] = "and #{array[-1]}"
     return array.join(", ")
   end
